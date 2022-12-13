d394d114782df3288c2bf75758e5505d0f273531
naga-tharun <nagatharunmakkena@gmail.com>
2022-12-13 19:07:42 +0530

    division not working

--- Assignment_lab_bonus/Bonus_problem_2/script.sh
+++ Assignment_lab_bonus/Bonus_problem_2/script.sh
@@ -1,8 +1,3 @@
-nasm -felf64 add.asm
-nasm -felf64 subtract.asm
-nasm -felf64 multiply.asm
-nasm -felf64 divide.asm
-gcc -S arithmetic.c
-as arithmetic.s -o arithmetic.o
-gcc add.o subtract.o multiply.o divide.o arithmetic.o -o output
+nasm -felf64 arithmetic.asm
+gcc arithmetic.o -o output -no-pie
 ./output
